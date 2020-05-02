0d52afb7a1fff52941ab0a983f6ddeb861ab2273
Patrick <patrick204nqh@gmail.com>
2020-05-02 12:50:44 +0700

    add Card component

--- README.md
+++ README.md
@@ -5,3 +5,17 @@
 - classnames
 - react-chartjs-2
 - react-countup
+
+3. Awesome function:
+
+- Fetch data and modified data
+
+      export const fetchData = async () => {
+        try {
+          const {
+            data: { confirmed, recovered, deaths, lastUpdate },
+          } = await axios.get(url);
+
+          return { confirmed, recovered, deaths, lastUpdate };
+        } catch (error) {}
+      };
--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import styles from './App.module.css';
 import { fetchData } from './api';
 
 export class App extends Component {
-  async componentDidMount() {
-    const data = await fetchData();
+  state = {
+    data: {},
+  };
 
-    console.log(data);
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
   }
   render() {
+    const { data } = this.state;
+
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
