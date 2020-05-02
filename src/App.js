23abcf6f299f1791753e40d4d2dcb3503b9bae9c
Patrick <patrick204nqh@gmail.com>
2020-05-02 15:22:07 +0700

    add feature show  barChar when select a country

--- src/App.js
+++ src/App.js
@@ -24,13 +24,13 @@ export class App extends Component {
   };
 
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
 
     return (
       <div className={styles.container}>
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart />
+        <Chart data={data} country={country} />
       </div>
     );
   }
